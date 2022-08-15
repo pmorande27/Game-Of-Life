a2c4ea353b9b6b520167f051edcdbaab06bf48c8
Morandé Pablo <pmorande27@gmail.com>
2022-08-15 23:31:18 +0200

    Add animations of new patterns

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ import patterns
 
 def main():
     options = ['Glider','Gosper Glider Gun','Hertz Oscillator','Kok\'s Galaxy','MWWS Synthesis','MWWS','pulsar','sailboat','Simkin Glider Gun']
-    
+    '''
     for pattern_name in options:
         pattern = patterns.get_patern('patterns/'+pattern_name+'.txt')      
         pattern = patterns.center_pattern(pattern,100,100)
         ani = Animation(pattern)
         ani.animation_save(300, pattern_name)
+    '''
+    pattern_name = 'Spider'
+    pattern = patterns.get_patern('patterns/'+pattern_name+'.txt')      
+    pattern = patterns.center_pattern(pattern,100,100)
+    ani = Animation(pattern)
+    ani.animation_save(300, pattern_name)
+
     
 
 
