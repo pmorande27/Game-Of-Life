4744e070050f299ea3c0e2869ae44e31b4dd01a7
Morandé Pablo <pmorande27@gmail.com>
2022-08-19 14:21:23 +0200

    Add pictures

--- main.py
+++ main.py
@@ -14,11 +14,26 @@ def main():
         ani = Animation(pattern)
         ani.animation_save(300, pattern_name)
     '''
+    '''
     pattern_name = 'Spider'
     pattern = patterns.get_patern('patterns/'+pattern_name+'.txt')      
     pattern = patterns.center_pattern(pattern,100,100)
     ani = Animation(pattern)
     ani.animation_save(300, pattern_name)
+    '''
+    '''
+    for pattern_name in options:
+        pattern = patterns.get_patern('patterns/'+pattern_name+'.txt') 
+        pattern = patterns.center_pattern(pattern,100,100)
+        plt.imshow(pattern, cmap='Greys')
+        plt.savefig('results/Pictures/'+pattern_name+'_pattern.png')
+    '''
+    pattern_name = 'Block'
+    pattern = patterns.get_patern('patterns/'+pattern_name+'.txt') 
+    pattern = patterns.center_pattern(pattern,10,10)
+    plt.imshow(pattern, cmap='Greys')
+    plt.savefig('results/Pictures/'+pattern_name+'_pattern.png')
+    
 
     
 
